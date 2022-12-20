46892763f7d6f7ccfdcbc2792f8a4caa5f3e27b2
Fabian Raeskinet <fabian.raeskinet@avanade.com>
2022-12-20 22:42:26 +0100

    add test for find disk

---
+++ MyDisk.Services.Tests/Disks/CreateDiskCommandHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using Moq;
+using MyDisk.Domain.Models;
+using MyDisk.Domain.Tests;
+using MyDisk.Infrastructure.Interfaces.IRepositories;
+using MyDisk.Services.Disks.Commands;
+using MyDisk.Services.Disks.Requests;
+
+namespace MyDisk.Services.Tests.Disks;
+
+public class CreateDiskCommandHandlerTestsShould
+{
+    [Theory, AutoDomainData]
+    public async Task CreateDisk
+    (
+        [Frozen] Mock<IDiskRepository> repositoryMock,
+        [Frozen] Disk disk,
+        CreateDiskCommandHandler sut,
+        CreateDiskRequest request
+    )
+    {
+        request.ReleaseDate = "2022-12-20";
+        repositoryMock.Setup(x => x.CreateDisk(disk)).Returns(disk.Id);
+
+        var result = await sut.Handle(request, It.IsAny<CancellationToken>());
+
+        repositoryMock.Verify(x => x.CreateDisk(It.IsAny<Disk>()), Times.Once);
+        result.Should().NotBeEmpty();
+    }
+}
---
+++ MyDisk.Services.Tests/Disks/GetDiskByNameQueryHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture.Xunit2;
+using AutoMapper;
+using FluentAssertions;
+using Moq;
+using MyDisk.Domain.Models;
+using MyDisk.Domain.Tests;
+using MyDisk.Infrastructure.Interfaces.IRepositories;
+using MyDisk.Services.Disks.DTOs;
+using MyDisk.Services.Disks.Queries;
+using MyDisk.Services.Disks.Requests;
+
+namespace MyDisk.Services.Tests.Disks;
+
+public class GetDiskByNameQueryHandlerTestsShould
+{
+    [Theory, AutoDomainData]
+    public async Task FindDisk
+    (
+        [Frozen] Mock<IMapper> mapperMock,
+        [Frozen] Mock<IDiskRepository> repositoryMock,
+        GetDiskByNameQueryHandler sut,
+        GetDiskByNameRequest request,
+        Disk disk
+    )
+    {
+        repositoryMock.Setup(x => x.GetDiskByFilter(It.IsAny<Func<Disk, bool>>())).Returns(disk);
+
+        var result = await sut.Handle(It.IsAny<GetDiskByNameRequest>(), It.IsAny<CancellationToken>());
+        
+        mapperMock.Verify(x => x.Map<DiskResponse>(disk), Times.Once);
+        result.Should().NotBeNull();
+    }
+    
+    [Theory, AutoDomainData]
+    public async Task NotFindDisk
+    (
+        [Frozen] Mock<IMapper> mapperMock,
+        [Frozen] Mock<IDiskRepository> repositoryMock,
+        GetDiskByNameQueryHandler sut
+    )
+    {
+        repositoryMock.Setup(x => x.GetDiskByFilter(It.IsAny<Func<Disk, bool>>())).Returns(() => null);
+
+        var result = await sut.Handle(It.IsAny<GetDiskByNameRequest>(), It.IsAny<CancellationToken>());
+        
+        mapperMock.Verify(x => x.Map<DiskResponse>(It.IsAny<Disk>()), Times.Never);
+        result.Should().BeNull();
+    }
+}
