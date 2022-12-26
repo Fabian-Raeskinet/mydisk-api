9379ba5dc4a5d72c8e0987dcfb5c39f757a453f7
Fabian Raeskinet <fabian.raeskinet@avanade.com>
2022-12-26 16:53:48 +0100

    refactor api services config, add newtonsoft config

--- src/MyDisk.Api/DependencyInjection.cs
+++ src/MyDisk.Api/DependencyInjection.cs
@@ -9,19 +9,27 @@ public static class DependencyInjection
     public static void AddApiServices(this IServiceCollection services)
     {
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
-        services.AddApplicationMvc();
+        services.AddMvcConfiguration();
+        services.AddControllerConfiguration();
+        services.AddSwaggerConfiguration();
     }
 
-    private static void AddApplicationMvc(this IServiceCollection services)
+    private static void AddMvcConfiguration(this IServiceCollection services)
+    {
+        services.AddMvc(options => { options.Filters.Add<ApiExceptionFilterAttribute>(); })
+            .AddFluentValidation(x => x.AutomaticValidationEnabled = false);
+    }
+
+    private static void AddControllerConfiguration(this IServiceCollection services)
     {
         services.AddControllers()
             .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true)
-            .AddNewtonsoftJson(options =>
-            {
-                options.SerializerSettings.Converters.Add(new StringEnumConverter());
-            });
-        services.AddMvc(options => { options.Filters.Add<ApiExceptionFilterAttribute>(); })
-            .AddFluentValidation(x => x.AutomaticValidationEnabled = false);
+            .AddNewtonsoftJson(options => { options.SerializerSettings.Converters.Add(new StringEnumConverter()); });
+    }
+
+    private static void AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        services.AddSwaggerGen();
+        services.AddSwaggerGenNewtonsoftSupport();
     }
 }
