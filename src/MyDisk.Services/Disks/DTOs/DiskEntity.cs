e59fe7f0d5ecbc0877b8d3da0600aaf804ec8742
Fabian Raeskinet <fabian.raeskinet@avanade.com>
2022-12-26 12:53:45 +0100

    update DTO

--- src/MyDisk.Services/Disks/DTOs/DiskEntity.cs
+++ src/MyDisk.Services/Disks/DTOs/DiskEntity.cs
@@ -1,4 +1,5 @@
 ﻿using MyDisk.Domain.Entities;
+using MyDisk.Services.Authors.DTOs;
 
 namespace MyDisk.Services.Disks.DTOs;
 
@@ -6,8 +7,7 @@ public class DiskEntity
 {
     public Guid Id { get; set; }
     public string? Name { get; set; }
-    public DateTime CreatedDateTime { get; set; }
     public DateTime? ReleaseDate { get; set; }
     public string? ImageUrl { get; set; }
-    public Author? Author { get; set; }
+    public AuthorResponse? Author { get; set; }
 }
