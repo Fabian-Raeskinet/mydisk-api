0d05606c4292139f30d5226ee865b614ab7adf89
Fabian Raeskinet <fabian.raeskinet@avanade.com>
2022-12-26 15:53:35 +0100

    add Validators

--- src/MyDisk.Services/Disks/Requests/DeleteDiskRequest.cs
+++ src/MyDisk.Services/Disks/Requests/DeleteDiskRequest.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using MyDisk.Services.Common.Enums;
 
 namespace MyDisk.Services.Disks.Requests;
@@ -7,4 +8,11 @@ public class DeleteDiskRequest : IRequest<Unit>
 {
     public DeleteDiskByPropertyEnum? Property { get; set; }
     public string? Value { get; set; }
+}
+
+public class DeleteDiskRequestValidator : AbstractValidator<DeleteDiskRequest>
+{
+    public DeleteDiskRequestValidator() { 
+        RuleFor(x => x.Value).NotEmpty().NotNull();
+    }
 }
--- src/MyDisk.Services/Disks/Requests/UpdateDiskRequest.cs
+++ src/MyDisk.Services/Disks/Requests/UpdateDiskRequest.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using MyDisk.Services.Disks.DTOs;
 
 namespace MyDisk.Services.Disks.Requests;
@@ -8,4 +9,12 @@ public class UpdateDiskRequest : IRequest<DiskResponse>
     public Guid? Id { get; set; }
     public string? Name { get; set; }
     public DateTime? ReleaseDate { get; set; }
+}
+
+public class UpdateDiskRequestValidator : AbstractValidator<UpdateDiskRequest>
+{
+    public UpdateDiskRequestValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().NotNull();
+    }
 }
