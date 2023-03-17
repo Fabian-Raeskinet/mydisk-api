9bf7387ffdf0f8375c385405c42780cfbf9dbdbf
Fabian Raeskinet <fabian.raeskinet@avanade.com>
2023-03-17 22:01:58 +0100

    refactor: migrate from private to public prop

--- MyDisk.Api/Controllers/MiscellaneousController.cs
+++ MyDisk.Api/Controllers/MiscellaneousController.cs
@@ -8,19 +8,19 @@ namespace MyDisk.Api.Controllers;
 [ApiController]
 public class MiscellaneousController : ControllerBase
 {
-    private readonly IMediator _mediator;
-
     public MiscellaneousController(IMediator mediator)
     {
-        _mediator = mediator;
+        Mediator = mediator;
     }
-    
+
+    public IMediator Mediator { get; }
+
     [HttpGet]
     [Route("disks")]
     [ProducesResponseType(204)]
     public async Task<IActionResult> RetryServiceAction()
     {
-         await _mediator.Send(new RetryServiceRequest());
+        await Mediator.Send(new RetryServiceRequest());
         return NoContent();
     }
 }
