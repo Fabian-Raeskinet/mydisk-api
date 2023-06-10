a27a0dc3f146b13ce5c55c821f9f255c6af1d512
Fabian Raeskinet <fabian.raeskinet@avanade.com>
2023-06-11 00:05:38 +0200

    pipeline test

--- Contracts.Validators.Tests/Disks/UpdateDiskCommandValidatorFixture.cs
+++ Contracts.Validators.Tests/Disks/UpdateDiskCommandValidatorFixture.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Contracts.Validators.Disks;
 using FluentAssertions;
 using MyDisk.Services.Disks;
@@ -7,20 +8,20 @@ namespace Contracts.Validators.Tests.Disks;
 
 public class UpdateDiskCommandValidatorFixture
 {
-    // [Theory]
-    // [AutoServiceData]
-    // public async Task ShouldThrowValidationException(string name)
-    // {
-    //     // Arrange
-    //     var request = new UpdateDiskCommandRequest
-    //         { Name = name, ReleaseDate = DateTime.Parse("30-07-23 16:01:55"), Id = Guid.Empty };
-    //
-    //     // Act
-    //     var act = await new UpdateDiskCommandValidator().ValidateAsync(request);
-    //
-    //     // Assert
-    //     act.IsValid.Should().BeFalse();
-    // }
+    [Theory]
+    [AutoServiceData]
+    public async Task ShouldThrowValidationException(string name)
+    {
+        // Arrange
+        var request = new UpdateDiskCommandRequest
+            { Name = name, ReleaseDate = DateTime.Parse("30-07-23 16:01:55", CultureInfo.InvariantCulture), Id = Guid.Empty };
+    
+        // Act
+        var act = await new UpdateDiskCommandValidator().ValidateAsync(request);
+    
+        // Assert
+        act.IsValid.Should().BeFalse();
+    }
 
     [Theory]
     [AutoServiceData]
