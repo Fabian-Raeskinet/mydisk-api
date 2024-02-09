4cc6e68d693f70ccc4d8f2ef42f83df48ad3b6d4
Fabian Raeskinet <fabian.raeskinet@anytrix.be>
2024-02-09 23:03:46 +0100

    Remove unnecessary using directives in Author.cs
    The `System.ComponentModel.DataAnnotations` and `System.ComponentModel.DataAnnotations.Schema` namespaces were being imported but not used in the Author.cs file. This commit removes these unnecessary using directives to clean up the code.

--- Domain/Authors/Author.cs
+++ Domain/Authors/Author.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace MyDisks.Domain.Authors;
+﻿namespace MyDisks.Domain.Authors;
 
 public class Author : AggregateRoot<Guid>
 {
